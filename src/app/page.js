4ff7f1f97b4979d78f2eb40f9df43925d5e5ccd4
Veenna <veenna@graduate.utm.my>
2026-06-25 15:43:54 +0800

    add home redirect to login

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-    return (
-      <div style={{ padding: "40px", fontFamily: "Arial" }}>
-        <h1>Smart Employee Portal 🚀</h1>
-        <p>System is running successfully</p>
-      </div>
-    )
-  }
+  redirect('/login')
+}
