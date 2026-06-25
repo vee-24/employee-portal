764788e1d35803f22d4b55961377a8ab1d8fd255
Veenna <veenna@graduate.utm.my>
2026-06-25 14:44:13 +0800

    add homepage route fix 404

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div style={{ padding: "40px", fontFamily: "Arial" }}>
+        <h1>Smart Employee Portal 🚀</h1>
+        <p>System is running successfully</p>
+      </div>
+    )
+  }
